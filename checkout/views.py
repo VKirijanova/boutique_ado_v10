06af5f527e74489f672aaef7dac5a73117b83b68
VKirijanova <v.kirijanova@gmail.com>
2021-09-08 15:20:16 +0000

    completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -9,11 +9,12 @@ from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
+import json
 
 @require_POST
 def cache_checkout_data(request):
     try:
-        pid = request.POST.GET('client_secret').split('_secret')[0]
+        pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
             'bag': json.dumps(request.session.get('bag', {})),
@@ -22,9 +23,10 @@ def cache_checkout_data(request):
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request,'Sorry, your payment cannot be \
-            proceed right now. Please try again later.' )
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -46,7 +48,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
